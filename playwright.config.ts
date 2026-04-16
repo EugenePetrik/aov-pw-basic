c4df5e2a84e740cc472d38707bdc30845afb6b6b
ypetryk <yevhen.petryk@burberry.com>
2026-04-16 21:06:13 +0300

    Add testIdAttribute configuration to Playwright settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,6 +44,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
+
+    testIdAttribute: 'data-test',
   },
 
   /* Configure projects for major browsers */
